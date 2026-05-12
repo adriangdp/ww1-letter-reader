2d224179d55fa56f5d78d414bd99451fdea3ef09
Adrián Gallardo <adriangdp@gmail.com>
2026-05-12 21:03:47 +0200

    refactor: return original string if short and no linebreaks
    JSON file does not have single paragraph letters under 250 characters, but if the dataset ever gets updated this will avoid errors.

--- src/utils/truncateText.ts
+++ src/utils/truncateText.ts
@@ -1,5 +1,8 @@
 export default function truncate(text:string) :string{
     const newLineMark = text.indexOf("\n");
+    if(newLineMark === -1 && text.length < 250){
+        return text;
+    }
     const cuttOffIndex = newLineMark > 0 && newLineMark < 250 ? newLineMark : 250;
     return text.slice(0,cuttOffIndex)+" [...]";
 }
